8723a57924369b907346107a8fa015fdd2796604
Javier Gonzalez <jgromeo93@gmail.com>
2021-03-13 16:11:07 +0100

    I applied some changes needs follow up

--- script.js
+++ script.js
@@ -1,44 +1,50 @@
-let firstValue = parseInt(document.getElementById("number1").value);
-let operator = document.getElementById("operator");
-let secondValue = parseInt(document.getElementById("number2").value);
+// button variable and events
+const calculateButton = document.getElementById('button')
+const result = document.querySelector('.total');
 
-console.log("firstValue", firstValue);
-console.log("operator", operator);
-console.log("secondValue", secondValue);
+calculateButton.addEventListener('click', function(){
+    // First value variable
+    const firstValue = parseInt(document.getElementById("firstValue").value)
+    // operator
+    const operator = document.getElementById("operator").value;
+    //second value variable
+    const secondValue = parseInt(document.getElementById("secondValue").value);
+    // final value
+    let finalValue = "";
+
+    // new result
+    // operator
+    switch (operator){
+        case "+":
+            console.log(firstValue + secondValue);
+            finalValue = firstValue + secondValue;
+            break;
+        case "-":
+            console.log(firstValue - secondValue);
+            finalValue = firstValue - secondValue;
+            break;
+        case "/":
+            console.log(firstValue / secondValue);
+            finalValue = firstValue / secondValue;
+            break;
+        case "*":
+            console.log(firstValue * secondValue);
+            finalValue = firstValue * secondValue;
+            break;
+        default:
+            console.log("Values are incorrect");
+            break;
+    
+    }
+    // result class toggle
+    result.innerHTML = `Result: ${finalValue}`;
+    document.getElementById("firstValue").value = 0;
+    document.getElementById("secondValue").value = 0;
+
+})
 
 
-switch (operator){
-    case "+":
-        console.log(firstValue + secondValue);
-        break;
-    case "-":
-        console.log(firstValue - secondValue);
-        break;
-    case "/":
-        console.log(firstValue / secondValue);
-        break;
-    case "*":
-        console.log(firstValue * secondValue);
-        break;
-    default:
-        console.log("Values are incorrect");
-        break;
-
-}
-console.log("Hello world!")
+console.log(`${firstValue - secondValue}`);
 
 
 
-/*Listen to user interaction on a button
-At this step, when you enter a new operation, console.log() does not show any result. However if you refresh your page,
- input values are kept and the result will be correct.
-In order to have a more user friendly approach, we will add a new "calculate" button. 
-
-When user will click on this button, it will retrieve current input values and launch the arithmetic operation.
-First, add this new "calculate" button in index.html.
-
-Then, in script.js, you have to listen when the user will click on this button. 
-To do that, you have to attach an eventListener to the element you want to monitor. 
-An eventListener is a function which will 
-be executed when a specific event is triggered on a specific element. 
-What is an event ? it could be a click, a mouse hovering an element, a double click, a keyboard interaction, a scroll, etc. Exhaustive list of events.*/
